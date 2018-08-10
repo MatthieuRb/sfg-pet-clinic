30a168c1576cf3b6adba7b9ac027bcc767911a38
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-08-10 10:23:03 +0200

    Refactor interfaces

--- pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/services/PetService.java
@@ -1,16 +1,8 @@
 package com.matthieurb.sfgpetclinic.services;
 
-import java.util.Set;
-
-import com.matthieurb.sfgpetclinic.model.Owner;
 import com.matthieurb.sfgpetclinic.model.Pet;
 
-public interface PetService {
-	
-	Pet findById(Long id);
-	
-	Pet save(Pet pet);
-	
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
+
 
 }
--- pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/services/VetService.java
@@ -1,17 +1,7 @@
 package com.matthieurb.sfgpetclinic.services;
 
-import java.util.Set;
-
-import com.matthieurb.sfgpetclinic.model.Owner;
-import com.matthieurb.sfgpetclinic.model.Pet;
 import com.matthieurb.sfgpetclinic.model.Vet;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long> {
 	
-	Vet findById(Long id);
-	
-	Vet save(Vet vet);
-	
-	Set<Vet> findAll();
-
 }
