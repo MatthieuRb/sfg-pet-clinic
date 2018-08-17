c37d7d6e52ac9dee6c512a6043307c5b6b94b574
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-08-17 16:25:50 +0200

    Add specialty

--- pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/model/Vet.java
@@ -1,5 +1,17 @@
 package com.matthieurb.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+	
+	private Set<Specialty> spetialties;
+
+	public Set<Specialty> getSpetialties() {
+		return spetialties;
+	}
+
+	public void setSpetialties(Set<Specialty> spetialties) {
+		this.spetialties = spetialties;
+	}
 
 }
