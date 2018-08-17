8a3f029f5b073ed8ee7a855bb28a2b6cd3e61144
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-08-17 09:43:00 +0200

    Add auto-generate ID values

--- pet-clinic-web/src/main/java/com/matthieurb/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/matthieurb/sfgpetclinic/bootstrap/DataLoader.java
@@ -28,14 +28,12 @@ public class DataLoader implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setPrenom("Michael");
 		owner1.setNom("Weston");
 		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setPrenom("Jean");
 		owner2.setNom("Claude");
 		
@@ -44,14 +42,12 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loeaded Owners ...");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setNom("Dupond");
 		vet1.setPrenom("Patrick");
 		
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setNom("Rambaud");
 		vet2.setPrenom("Mat");
 		
