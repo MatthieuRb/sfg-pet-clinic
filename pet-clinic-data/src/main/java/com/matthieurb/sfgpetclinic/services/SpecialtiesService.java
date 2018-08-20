8171644342372fb7629e7d2d3b34ac40540bcbc3
Matthieu Rambaud <rambaud.matthieu@gmail.com>
2018-08-20 10:03:16 +0200

    Add specialities map services

---
+++ pet-clinic-data/src/main/java/com/matthieurb/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.matthieurb.sfgpetclinic.services;
+
+import com.matthieurb.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+
+}
